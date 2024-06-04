ab8124725dce7cd64c145a6539bb9623d51d3426
malsulaimi <31324944+malsulaimi@users.noreply.github.com>
2024-06-04 21:18:49 +0300

    Add files via upload

---
+++ ChatModules.py
@@ -0,0 +1,50 @@
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_core.pydantic_v1 import BaseModel , Field
+import os  
+from langchain_core.tools import Tool , tool
+from langchain_core.messages import AIMessage , SystemMessage , HumanMessage
+from langchain_core.output_parsers import   StrOutputParser
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain.agents import create_react_agent , AgentExecutor , create_structured_chat_agent
+from langchain import hub
+from Tools import getInternetSearch , getProductsListFromSuperMarkets , getProductPriceBybarcodeWrapperFormLLM , getTivSearchResult
+os.environ['OPENAI_API_KEY'] = 'sk-proj-CSA0Un6wjcpP7ETJfxTzT3BlbkFJ77UjxeNNbssXJ790aEQi'
+os.environ['SUPERMARKETS_API_KEY'] = '7b9f775670607b87a957a6e4259e4f24'
+os.environ['TAVILY_API_KEY']  = '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'
+chatHistory=[]
+
+
+
+class Chatter : 
+    def __init__(self) :
+        text_template = """ given a person name {person_name} I need you to get me the linkedIn profile Page. your answer should contain URLs only. """
+        llm = ChatOpenAI(temperature=0 , model='gpt-4o')
+        agent_tools = [  
+        Tool(
+            name='get product prices from supermarket by bardcode' , 
+            description = """ this tool is usefull to get products price from supermarket based on Product barcode. Important arguments to this function must be product barcode such as 6271100990018""",
+            func=getProductPriceBybarcodeWrapperFormLLM
+        
+        ) , Tool(
+            name='LinkedIn Search' , 
+            description = """ this tool is usefull to search user linkedIn profiles. Important arguments to this function must be of the formt {"name":"person name" , "hits":" number of url hits"}""",
+            func=getTivSearchResult      
+        )  
+            , getProductsListFromSuperMarkets
+        ]
+
+        react_prompt = hub.pull('hwchase17/structured-chat-agent')
+        react_agent = create_structured_chat_agent(llm = llm , tools=agent_tools , prompt=react_prompt)
+        self.agent_executer = AgentExecutor(agent = react_agent , tools = agent_tools , verbose = False , handle_parsing_errors=False ,   maxIterations= 2 ) 
+
+    def  answerQustion(self , question, history) : 
+        if history !=None : 
+            human = HumanMessage(content =question)
+            history.append(human)
+            out = self.agent_executer.invoke({'input':question , 'chat_history':history})
+            ai = AIMessage(content=out['output'])
+            history.append(ai)
+        else : 
+            out = self.agent_executer.invoke({'input':question })
+        return out['output'] , history 
---
+++ Tools.py
@@ -0,0 +1,126 @@
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_core.pydantic_v1 import BaseModel , Field
+import os  
+from langchain_core.tools import Tool , tool
+from langchain_core.messages import AIMessage , SystemMessage , HumanMessage
+from langchain_core.output_parsers import   StrOutputParser
+from langchain_community.tools.tavily_search import TavilySearchResults
+
+
+import requests 
+import json
+
+
+os.environ['OPENAI_API_KEY'] = 'sk-proj-CSA0Un6wjcpP7ETJfxTzT3BlbkFJ77UjxeNNbssXJ790aEQi'
+os.environ['SUPERMARKETS_API_KEY'] = '7b9f775670607b87a957a6e4259e4f24'
+os.environ['TAVILY_API_KEY']  = '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'
+chatHistory=[]
+
+
+def getProductPriceByBarcodeFromMarket(productBarCode ,  marketName='ALL' , pricingSummerizationMethod='AVG') : 
+    """ this method retuns price of an item based on its barcode. The price is fetched from Kuwaiti Supermarkets"""
+    result = []
+    try : 
+        url = "http://daiyacoop.com/appadmin/api/get_product/{barcode}".format(barcode=productBarCode)
+        payload = {}
+        headers = {
+          'api-key': os.environ['SUPERMARKETS_API_KEY'],
+          'Cookie': 'ci_session=gc9isema4iojitf8ef05rh69b6iu4ft1'
+        }
+        response = requests.request("GET", url, headers=headers, data=payload)
+        if 200==response.status_code : 
+            jdata = json.loads(response.text )
+            pstatus = jdata['status']
+            if bool(pstatus) : 
+                pdata = jdata['data'][0]
+                pprice = pdata['price']
+                pdescrition = pdata['product_description']
+                result.append({'market_name':marketName , 'item_name':pdescrition, 'price':pprice,  'barcode':productBarCode })
+    except : 
+        result=[]
+    return result
+    
+#print(getProductPriceByBarcodeFromMarket(None))
+
+
+
+def getProductsListFromSuperMarketsBasedOnWord( productName) : 
+    """ this tool returns """
+    result = ''
+    try : 
+        url = "http://daiyacoop.com/appadmin/api/search_product"
+        payload = {'query':productName}
+        headers = {
+          'api-key': os.environ['SUPERMARKETS_API_KEY'],
+          'Cookie': 'ci_session=gc9isema4iojitf8ef05rh69b6iu4ft1'
+        }
+        response = requests.request("POST", url, headers=headers, data=payload)
+        if 200==response.status_code : 
+            jdata = json.loads(response.text )
+            pstatus = jdata['status']
+            if bool(pstatus) : 
+                for x in jdata['data'] : 
+                    pdata = x
+                    pprice = pdata['price']
+                    pdescrition = pdata['product_description']
+                    barcode = pdata['barcode_number']
+                    msg="""product name is  : {item_name} , and it's price is : {price}"""
+                    result = result + msg.format( item_name=pdescrition, price=pprice ) + '\n'
+
+    except :
+        result = ''
+    return result    
+
+class SuperMarketByName(BaseModel) : 
+    productname : str =Field(description = ' this is the product name to search and get the price from market')                        
+
+@tool("get product info from supermarket by product name", args_schema = SuperMarketByName   )
+def getProductsListFromSuperMarkets(productname: str ) ->str : 
+    """ This tool is usfull to  search an Item name in supermarkets and return its price. This returns many possible matches and you must select what matches the customer input. its important to pass to the tool the argument as product name like such as Cheese"""
+    lis = productname.split() 
+    result = ''
+    for x in lis : 
+       re= getProductsListFromSuperMarketsBasedOnWord(x)
+       result = result + re + '\n'
+    return result
+
+def getProductPriceBybarcodeWrapperFormLLM(productBarcode:str) ->str: 
+    #print("checking : " , productBarcode)
+    response = getProductPriceByBarcodeFromMarket(productBarcode ,  marketName='ALL' , pricingSummerizationMethod='AVG') 
+    price = '0' 
+    try : 
+        data = response[0] 
+        price = data['price'] ##"product name is {name} and its price is {price}".format(price=data['price'],name=data['item_name'])
+        price = "Price returned is : {prc}".format(prc=price)
+    except :
+        price = '0'
+    return price 
+
+#price = getProductPriceBybarcodeWrapperFormLLM('8000815004776')
+
+
+def getInternetSearch(searchQuery) : 
+
+    search = TavilySearchResults()
+    res = search.run(searchQuery) 
+    return res
+
+#getInternetSearch("what is happening in Kuwait")
+
+
+def getTivSearchResult(strdict) : 
+    dic = json.loads(strdict)
+    name = dic['name']
+    hits = int(dic['hits'])
+    search = TavilySearchResults()
+    res = search.run(f"{name} linked full profile page") 
+    results = ''
+    for x in range(hits) : 
+       try : 
+        results = results+res[x]['url'] + "\n"
+       except : 
+           break
+    return results 
+
+getTivSearchResult("""{"name":"Mousa Alsulaimi" , "hits":1}""")
