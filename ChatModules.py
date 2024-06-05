7fa168d55e10bf5145b9f360789d8568ae376ab8
malsulaimi <31324944+malsulaimi@users.noreply.github.com>
2024-06-05 17:53:02 +0300

    Update ChatModules.py

--- ChatModules.py
+++ ChatModules.py
@@ -14,7 +14,7 @@ os.environ['OPENAI_API_KEY'] =  st.secrets["OPENAI_API_KEY"]
 os.environ['SUPERMARKETS_API_KEY'] =  st.secrets["SUPERMARKETS_API_KEY"]
 os.environ['TAVILY_API_KEY']  =  st.secrets["TAVILY_API_KEY"]
 os.environ['LANGCHAIN_API_KEY']  =  st.secrets["LANGCHAIN_API_KEY"]
-os.environ['LANGCHAIN_TRACING_V2']  = True
+os.environ['LANGCHAIN_TRACING_V2']  = "true"
 os.environ['LANGCHAIN_PROJECT']  = "JasimGPT"
 chatHistory=[]
 
